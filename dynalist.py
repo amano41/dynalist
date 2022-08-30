c1508522d09e5f2b486dc8326aae6b16f6af5d50
Amano, Yoichi <amano.yoichi@gmail.com>
2022-08-30 09:04:44 +0900

    Improve readability

--- dynalist.py
+++ dynalist.py
@@ -16,11 +16,15 @@ class Dynalist:
         self.token = token
 
     def _post(self, method: str, json_data: dict) -> dict:
+        # fmt: off
+        headers = {
+            "Content-Type": "application/json",
+            "User-Agent": "Mozilla/5.0"
+        }
+        # fmt: on
 
         url = self.END_POINT + method
         data = json.dumps(json_data).encode("utf-8")
-        headers = {"Content-Type": "application/json", "User-Agent": "Mozilla/5.0"}
-
         request = Request(url, data, headers)
 
         try:
@@ -31,69 +35,98 @@ class Dynalist:
             raise
         except URLError:
             raise
-
         if result["_code"].lower() != "ok":
             raise RuntimeError(result["_msg"])
 
         return result
 
     def list_files(self) -> dict:
-
+        # fmt: off
         method = "file/list"
-        json_data = {"token": self.token}
-
+        json_data = {
+            "token": self.token
+        }
         return self._post(method, json_data)
+        # fmt: on
 
     def edit_file(self, changes: list[dict]) -> dict:
-
+        # fmt: off
         method = "file/edit"
-        json_data = {"token": self.token, "changes": changes}
-
+        json_data = {
+            "token": self.token,
+            "changes": changes
+        }
         return self._post(method, json_data)
+        # fmt: on
 
     def move_file(self, file_type: Literal["folder", "document"], file_id: str, parent_id: str, index: int) -> dict:
-
-        changes = [{"action": "move", "type": file_type, "file_id": file_id, "parent_id": parent_id, "index": index}]
-
+        # fmt: off
+        changes = [{
+            "action": "move",
+            "type": file_type,
+            "file_id": file_id,
+            "parent_id": parent_id,
+            "index": index
+        }]
         return self.edit_file(changes)
+        # fmt: on
 
     def rename_file(self, file_type: Literal["folder", "document"], file_id: str, title: str) -> dict:
-
-        changes = [{"action": "edit", "type": file_type, "file_id": file_id, "title": title}]
-
+        # fmt: off
+        changes = [{
+            "action": "edit",
+            "type": file_type,
+            "file_id": file_id,
+            "title": title
+        }]
         return self.edit_file(changes)
+        # fmt: on
 
     def create_file(
         self, file_type: Literal["folder", "document"], parent_id: str, index: int, title: Optional[str] = None
     ) -> dict:
-
-        changes = [{"action": "create", "type": file_type, "parent_id": parent_id, "index": index}]
-
+        # fmt: off
+        changes = [{
+            "action": "create",
+            "type": file_type,
+            "parent_id": parent_id,
+            "index": index
+        }]
         if title is not None:
             changes[0]["title"] = title
-
         return self.edit_file(changes)
+        # fmt: on
 
     def read_doc(self, document_id: str) -> dict:
-
+        # fmt: off
         method = "doc/read"
-        json_data = {"token": self.token, "file_id": document_id}
-
+        json_data = {
+            "token": self.token,
+            "file_id": document_id
+        }
         return self._post(method, json_data)
+        # fmt: on
 
     def check_for_updates(self, document_ids: list[str]) -> dict:
-
+        # fmt: off
         method = "doc/check_for_updates"
-        json_data = {"token": self.token, "file_ids": document_ids}
-
+        json_data = {
+            "token": self.token,
+            "file_ids": document_ids
+        }
         return self._post(method, json_data)
+        # fmt: on
 
     def edit_doc(self, document_id: str, changes: list[dict]) -> dict:
-
+        # fmt: off
         method = "doc/edit"
-        json_data = {"token": self.token, "file_id": document_id, "changes": changes}
-
+        json_data = {
+            "token": self.token,
+            "file_id": document_id,
+            "changes": changes
+        }
         return self._post(method, json_data)
+        # fmt: off
 
     def insert_node(
         self,
@@ -107,9 +140,8 @@ class Dynalist:
         heading: int = 0,
         color: int = 0,
     ) -> dict:
-
-        changes = [
-            {
+        # fmt: off
+        changes = [{
                 "action": "insert",
                 "parent_id": parent_node_id,
                 "index": index,
@@ -118,13 +150,11 @@ class Dynalist:
                 "checkbox": checkbox,
                 "heading": heading,
                 "color": color,
-            }
-        ]
-
+        }]
         if note is not None:
             changes[0]["note"] = note
-
         return self.edit_doc(document_id, changes)
+        # fmt: on
 
     def update_node(
         self,
@@ -137,37 +167,41 @@ class Dynalist:
         heading: int = 0,
         color: int = 0,
     ) -> dict:
-
-        changes = [
-            {
+        # fmt: off
+        changes = [{
                 "action": "edit",
                 "node_id": target_node_id,
                 "checked": checked,
                 "checkbox": checkbox,
                 "heading": heading,
                 "color": color,
-            }
-        ]
-
+        }]
         if content is not None:
             changes[0]["content"] = content
-
         if note is not None:
             changes[0]["note"] = note
-
         return self.edit_doc(document_id, changes)
+        # fmt: on
 
     def move_node(self, document_id: str, target_node_id: str, parent_node_id: str, index: int) -> dict:
-
-        changes = [{"action": "move", "node_id": target_node_id, "parent_id": parent_node_id, "index": index}]
-
+        # fmt: off
+        changes = [{
+            "action": "move",
+            "node_id": target_node_id,
+            "parent_id": parent_node_id,
+            "index": index
+        }]
         return self.edit_doc(document_id, changes)
+        # fmt: on
 
     def delete_node(self, document_id: str, target_node_id: str) -> dict:
-
-        changes = [{"action": "delete", "node_id": target_node_id}]
-
+        # fmt: off
+        changes = [{
+            "action": "delete",
+            "node_id": target_node_id
+        }]
         return self.edit_doc(document_id, changes)
+        # fmt: on
 
     def add_to_inbox(
         self,
@@ -179,7 +213,7 @@ class Dynalist:
         heading: int = 0,
         color: int = 0,
     ) -> dict:
-
+        # fmt: off
         method = "inbox/add"
         json_data = {
             "token": self.token,
@@ -190,29 +224,40 @@ class Dynalist:
             "heading": heading,
             "color": color,
         }
-
         if note is not None:
             json_data["note"] = note
-
         return self._post(method, json_data)
+        # fmt: on
 
     def upload_file(self, file_name: str, content_type: str, base64_data: str) -> dict:
-
+        # fmt: off
         method = "upload"
-        json_data = {"token": self.token, "filename": file_name, "content_type": content_type, "data": base64_data}
-
+        json_data = {
+            "token": self.token,
+            "filename": file_name,
+            "content_type": content_type,
+            "data": base64_data
+        }
         return self._post(method, json_data)
+        # fmt: on
 
     def get_pref(self, key: Literal["inbox_location", "inbox_move_position"]) -> dict:
-
+        # fmt: off
         method = "pref/get"
-        json_data = {"token": self.token, "key": key}
-
+        json_data = {
+            "token": self.token,
+            "key": key
+        }
         return self._post(method, json_data)
+        # fmt: on
 
     def set_pref(self, key: Literal["inbox_location", "inbox_move_position"], value: str) -> dict:
-
+        # fmt: off
         method = "pref/set"
-        json_data = {"token": self.token, "key": key, "value": value}
-
+        json_data = {
+            "token": self.token,
+            "key": key,
+            "value": value
+        }
         return self._post(method, json_data)
+        # fmt: on
